a867375553bc82576c6a297c4d4a0871190ed5fa
rherugu <rherugu@gmail.com>
2020-03-15 10:45:27 -0400

    added new comment

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React, { Component } from 'react';
-
+// app.js code
 import {
   StyleSheet,
   Text,
