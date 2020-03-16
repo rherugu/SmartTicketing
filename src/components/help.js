369319097ccca8abb236f8112e3690853bc341b6
rherugu <rherugu@gmail.com>
2020-03-16 10:49:04 -0400

    Minor style changes agian

--- src/components/help.js
+++ src/components/help.js
@@ -32,9 +32,89 @@ export default class help extends Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-        <View>
-            <Text>hi</Text>
-        </View>
+      <View style={styles.container}>
+      <Text style={styles.heading}>Help</Text>
+      <View style={styles.container1}>
+      </View>
+      
+    </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+    opacity: 1,
+    
+  },
+  container1: {
+    opacity: 1,
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+      width:100,
+      color: 'black',
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
+  },
+  btn: {
+    borderRadius: 29,
+    backgroundColor: '#DDDDDD',
+  },
+  btntxt: {
+    color: "#111111"
+  },
+  buttonContainer2: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 0,
+    width:250,
+    borderRadius:30,
+  },
+  button: {
+    backgroundColor: '#DDDDDD',
+  },
+  heading: {
+    fontSize: 24,
+  },
+
+});
